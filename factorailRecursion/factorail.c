5843e9031e6a236136ffd493b05fd085832238ad
user <user@DESKTOP-M7F2LBS>
2023-04-25 11:50:08 +0900

    초기 커밋.

---
+++ factorailRecursion/factorail.c
@@ -0,0 +1,20 @@
+﻿
+#include <stdio.h>
+
+int factorial(int n)
+{
+	if (n == 1) return 1;
+	else return (n * factorial(n - 1));
+}
+
+int factorial_iter(int n)
+{
+	int k, result = 1;
+	for (k = n;k > 0;k--)
+		result = result * k;
+	return result;
+}
+
+int main()
+{
+}
