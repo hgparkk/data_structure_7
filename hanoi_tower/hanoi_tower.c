988f4ee34afc1d0bb27b678d45b8649e492e766c
hgparkk <phg2559@naver.com>
2023-05-04 12:29:03 +0900

    new commit

---
+++ hanoi_tower/hanoi_tower.c
@@ -0,0 +1,25 @@
+﻿/*
+프로그램 내용 : 하노이의 탑 문제
+실습일 : 2023.04.25
+실습자 : 박현규
+학번 : 202111678
+*/
+
+#include <stdio.h>
+
+void hanoi_tower(int n, char from, char tmp, char to)
+{
+    if (n == 1)
+        printf("원판 1을 %c에서 %c으로 옮긴다.\n", from, to);
+    else
+    {
+        hanoi_tower(n - 1, from, to, tmp);
+        printf("원판 %d을 %c에서 %c으로 옮긴다.\n", n, from, to);
+        hanoi_tower(n - 1, tmp, from, to);
+    }
+}
+
+int main()
+{
+    hanoi_tower(4, 'A', 'B', 'C');
+}
