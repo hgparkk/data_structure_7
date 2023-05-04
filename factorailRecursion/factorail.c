6fa9904da1ce1c0b9c940cdc1fcdecec10bcc8f4
hgparkk <phg2559@naver.com>
2023-05-04 11:09:59 +0900

    new commit

--- factorailRecursion/factorail.c
+++ factorailRecursion/factorail.c
@@ -1,8 +1,15 @@
-﻿
+﻿/*
+프로그램 내용 : 순환함수와 반복함수를 이용한 팩토리얼 계산
+실습일 : 2023.04.25
+실습자 : 박현규
+학번 : 202111678
+*/
+
 #include <stdio.h>
 
 int factorial(int n)
 {
+	printf("factorial(%d)\n", n);
 	if (n == 1) return 1;
 	else return (n * factorial(n - 1));
 }
@@ -17,4 +24,5 @@ int factorial_iter(int n)
 
 int main()
 {
+	factorial(3);
 }
